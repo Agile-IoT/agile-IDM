f106ae83d0152df64cc8bac611e8cae351f099c3
Eduard <brehmedu@fim.uni-passau.de>
2017-07-04 08:15:39 -0400

    Changed examples and added response in delete function

--- routes/policies/pap.js
+++ routes/policies/pap.js
@@ -10,8 +10,8 @@ var idmcore;
 
 function RouterApi(tokenConf, idmcore, router) {
 
-    //example to call tthis one
-    // curl -H "Authorization: bearer nNGNryDDZ4zQYeWYGYcnOdxJ90k9s6" 'http://localhost:3000/api/v1/entity/user/bob!@!agile-local'
+    //example to call this one
+    // curl -H "Authorization: bearer nNGNryDDZ4zQYeWYGYcnOdxJ90k9s6" 'http://localhost:3000/api/v1/pap/sensor/sensor-id'
     //returns entity with 200 if OK, else, it can return 404 if the entity is not found, 401 or 403 in case of security errors or 500 in case of unexpected situations
     router.route('/pap/:entity_type/:entity_id').get(
         passport.authenticate('agile-bearer', {
@@ -70,7 +70,7 @@ function RouterApi(tokenConf, idmcore, router) {
     );
 
     //returns 200 and the entity, or 401 or 403, in case of security issues, 422 in case a user is attempted to be created through this API, or 409 if entity already exists, 500 in case of unexpected situations
-    //curl -H "Content-type: application/json" -H "Authorization: bearer HeTxINCpXD0U6g27D7AIxc2CvfFNaZ" -X DELETE 'http://localhost:3000/api/v1/entity/sensor/1/attribute/name'
+    //curl -H "Content-type: application/json" -H "Authorization: bearer HeTxINCpXD0U6g27D7AIxc2CvfFNaZ" -XDELETE 'http://localhost:3000/api/v1/pap/sensor/mysensor/policy/name'
     router.route('/pap/:entity_type/:entity_id/policy/:policy_name').delete(
         passport.authenticate('agile-bearer', {
             session: false
@@ -80,7 +80,7 @@ function RouterApi(tokenConf, idmcore, router) {
             var entity_type = "/" + req.params.entity_type;
             var entity_id = req.params.entity_id;
             var policy_name = req.params.policy_name;
-
+            res.json({id: entity_id, type: entity_type, name: policy_name});
           /* TODO: implement delete Entity Policy in agile-idm-core api
            // check if policy is an attribute_level_policy
            idmcore.deleteEntityPolicy(req.user, entity_id, entity_type, policy_name)
